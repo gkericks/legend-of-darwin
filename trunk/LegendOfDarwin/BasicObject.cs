8aa5f067a05c1f2d7cdc2b2a1ae1250da825358d
mehlicious@gmail.com <mehlicious@gmail.com@a3afd2dd-6ea3-eacb-4b53-07fd98ab0ad6>
2011-03-01 00:52:28 +0000

    added basicobject. Every model should extend this if its in the play area

---
+++ trunk/LegendOfDarwin/BasicObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace LegendOfDarwin
+{
+    class BasicObject
+    {
+        public Rectangle source;
+        public int X;
+        public int Y;
+        
+        public BasicObject(Rectangle sourceIn)
+        {
+            source = new Rectangle(sourceIn.X, sourceIn.Y, sourceIn.Width, sourceIn.Height);
+        }
+
+        public void setGridPosition(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+    }
+}
