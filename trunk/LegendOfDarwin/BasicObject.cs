505c069d0935dfab3b1174c8868c876b1f523112
mehlicious@gmail.com <mehlicious@gmail.com@a3afd2dd-6ea3-eacb-4b53-07fd98ab0ad6>
2011-03-02 03:56:34 +0000

    Added comments, did a bit better colision detection with another test darwin.

--- trunk/LegendOfDarwin/BasicObject.cs
+++ trunk/LegendOfDarwin/BasicObject.cs
@@ -8,9 +8,12 @@ namespace LegendOfDarwin
 {
     class BasicObject
     {
+        // The x coordinate to be put on the GameBoard class
         public int X;
+        // The y coordinate to be put on the GameBoard class
         public int Y;
 
+        // Set their positions
         public void setGridPosition(int x, int y)
         {
             X = x;
--- trunk/LegendOfDarwin/GameBoard.cs
+++ trunk/LegendOfDarwin/GameBoard.cs
@@ -8,13 +8,28 @@ namespace LegendOfDarwin
 {
     class GameBoard
     {
+        // Length and width of each square/rectangle
         private int squareLength, squareWidth;
+
+        // 2d array representation of the game floor. 
+        // Each rectangle upon each grid position contains the necessary information for each tile to be placed on
         private Rectangle[,] grid;
+
+        // A flag of each grid to see if an object occupies it
         private Boolean[,] hasObject;
+
+        // Number of squares going down
         private int gridLength;
+
+        // Number of squares going right
         private int gridWidth;
 
-       
+        /*
+         * boardSize contains the x and y length and width of the whole gameboard.
+         * boardSize.X = 25 and boardSize.Y = 25 would translate to a 25 X 25 board
+         * windowSize contains the x and y length and width of the game's window's size.
+         * windowSize.X = 900 and windowSize.Y = 600 would translate to a 900 X 600 window size in pixels
+         */
         public GameBoard(Vector2 boardSize, Vector2 windowSize)
         {
             squareLength = (int)(windowSize.Y / boardSize.Y);
@@ -36,6 +51,13 @@ namespace LegendOfDarwin
             }
         }
 
+
+        /*
+         * x is the width or 'going right' position on the grid
+         * y is the length or 'going down' position on the grid
+         * x = 3, y = 5 would translate to 3 to the right and 5 down
+         * Both start at 0
+         */ 
         public Boolean isTaken(int x, int y)
         {
             if (x < 0 || x > gridWidth)
@@ -49,6 +71,10 @@ namespace LegendOfDarwin
             return hasObject[x,y];
         }
 
+        /*
+         * bo contains the coordinates on the grid to be checked to see if its opened or not
+         * returns true on open, false on not
+         */ 
         public Boolean isGridPositionOpen(BasicObject bo)
         {
             if (isTaken(bo.X, bo.Y))
@@ -61,16 +87,26 @@ namespace LegendOfDarwin
             return true;
         }
 
+        /*
+         * PreCon: The bo's coordinates have the actually position using the function isGridPositionOpen
+         * Gets the rectangle based on the bo's coordinates on the grid
+         */ 
         public Rectangle getPosition(BasicObject bo)
         {
             return grid[bo.X,bo.Y];
         }
 
+        /*
+         * Frees the coordinates on the grid based on bo's coordinates
+         */ 
         public void freePosition(BasicObject bo)
         {
             hasObject[bo.X,bo.Y] = false;
         }
 
+        /*
+         * Does the same as freePosition
+         */ 
         public void setGridPositionOpen(int x, int y)
         {
             if (x < 0 || x > gridWidth)
@@ -85,11 +121,17 @@ namespace LegendOfDarwin
             }
         }
 
+        /*
+         * Exactly what it says
+         */ 
         public int getSquareLength()
         {
             return this.squareLength;
         }
 
+        /*
+         * Exactly what it says
+         */ 
         public int getSquareWidth()
         {
             return this.squareWidth;
