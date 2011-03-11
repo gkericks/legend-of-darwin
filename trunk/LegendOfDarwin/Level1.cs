6b462678e8035c18696e342e25efa6c755a2323e
brittanymelnyk@gmail.com <brittanymelnyk@gmail.com@a3afd2dd-6ea3-eacb-4b53-07fd98ab0ad6>
2011-03-11 19:56:00 +0000

    added new side wall/ menu image for the time being

--- trunk/LegendOfDarwin/Level1.cs
+++ trunk/LegendOfDarwin/Level1.cs
@@ -184,8 +184,8 @@ namespace LegendOfDarwin
             Texture2D messagePic = mainGame.Content.Load<Texture2D>("messageBox");
 
             // Test
-            Texture2D basicGridTex = mainGame.Content.Load<Texture2D>("metal_tile");
-            Texture2D basicMenuTex = mainGame.Content.Load<Texture2D>("StaticPic/grid_menu_outline");
+            Texture2D basicGridTex = mainGame.Content.Load<Texture2D>("StaticPic/metal_tile");
+            Texture2D basicMenuTex = mainGame.Content.Load<Texture2D>("StaticPic/side_wall");
 
             Texture2D basicStairUpTex = mainGame.Content.Load<Texture2D>("StaticPic/stairsUp");
             Texture2D basicStairDownTex = mainGame.Content.Load<Texture2D>("StaticPic/stairsDown");
