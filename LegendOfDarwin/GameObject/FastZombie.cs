959e191dfc50fe0b60d377307fa1dceddf7e10a4
rizzen93 <rizzen93@gmail.com>
2011-03-14 19:27:44 +0000

    Initial commit for leaf and the fastzombie.

---
+++ LegendOfDarwin/GameObject/FastZombie.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LegendOfDarwin.GameObject
+{
+    class FastZombie : Zombie
+    {
+   
+        public static new int ZOMBIE_MOVE_RATE = 100;
+
+        public Boolean sleeping = true;
+
+        public FastZombie(int startX, int startY, int mymaxX, int myminX, int mymaxY, int myminY, GameBoard myboard) :
+            base(startX, startY, mymaxX, myminX, mymaxY, myminY, myboard)
+        {
+            
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public void wakeUp()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void runFast(Darwin darwin)
+        {
+
+        }
+
+    }
+}
---
+++ LegendOfDarwin/GameObject/Leaf.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace LegendOfDarwin.GameObject
+{
+    class Leaf : BasicObject
+    {
+
+        private Texture2D brokenLeaf;
+        private Texture2D wholeLeaf;
+
+        private Boolean leafBroken;
+       
+        public Leaf(int x, int y, GameBoard myboard)
+            : base(myboard)
+        {
+            this.leafBroken = false;
+
+        }
+
+        /// <summary>
+        /// Get whether or not this leaf is whole, or broken
+        /// </summary>
+        /// <returns>True for when the leaf is broken, and false for when it is whole.</returns>
+        public Boolean getLeafState()
+        {
+            return leafBroken;
+        }
+
+        /// <summary>
+        /// Once Darwin steps on the leaf, it is now broken
+        /// </summary>
+        public void breakLeaf()
+        {
+            leafBroken = true;
+        }
+
+    }
+}
