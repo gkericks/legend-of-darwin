d65eb3c55220f0d4bc02960e5f6eab04bca80094
mehlicious@gmail.com <mehlicious@gmail.com@a3afd2dd-6ea3-eacb-4b53-07fd98ab0ad6>
2011-03-14 20:18:18 +0000

    Added potion. Not yet fully working

---
+++ trunk/LegendOfDarwin/GameObject/Potion.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using LegendOfDarwin.MenuObject;
+
+namespace LegendOfDarwin.GameObject
+{
+    class Potion : BasicObject
+    {
+        private Texture2D potionTex;
+        private bool isConsumed;
+        private const int healthReplenished = 15;
+
+        public Potion(GameBoard board) : base(board)
+        {
+            isConsumed = false;
+        }
+
+        public void LoadContent(Texture2D potTex)
+        {
+            potionTex = potTex;
+        }
+
+        public void Update(GameTime gameTime, KeyboardState ks, Darwin darwin, ZombieTime zTime)
+        { 
+            // grab us the current
+            LegendOfDarwin.Darwin.Dir facing = darwin.facing;
+
+            if (ks.IsKeyDown(Keys.A))
+            {
+                // check switch position in relation to darwin's position + facing direction 
+                switch (facing)
+                {
+                    case (LegendOfDarwin.Darwin.Dir.Left):
+                        if (((this.X + 1) == darwin.X) && (this.Y == darwin.Y))
+                        {
+                            consumePotion(zTime);
+                        }
+                        break;
+                    case (LegendOfDarwin.Darwin.Dir.Right):
+                        if (((this.X - 1) == darwin.X) && (this.Y == darwin.Y))
+                        {
+                            consumePotion(zTime);
+                        }
+                        break;
+                    case (LegendOfDarwin.Darwin.Dir.Up):
+                        if ((this.X == darwin.X) && ((this.Y + 1) == darwin.Y))
+                        {
+                            consumePotion(zTime);
+                        }
+                        break;
+                    case (LegendOfDarwin.Darwin.Dir.Down):
+                        if ((this.X == darwin.X) && ((this.Y - 1) == darwin.Y))
+                        {
+                            consumePotion(zTime);
+                        }
+                        break;
+                }
+            }
+        }
+
+        private void consumePotion(ZombieTime zTime)
+        {
+            board.setGridPositionOpen(this);
+            isConsumed = true;
+
+            int updateTime = zTime.getTime() + healthReplenished;
+            zTime.setTime(updateTime);
+
+        }
+
+        public void reset()
+        {
+            isConsumed = false;
+        }
+
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if (isConsumed == false)
+            {
+                spriteBatch.Draw(potionTex, this.destination, Color.White);
+            }
+        }
+    }
+}
