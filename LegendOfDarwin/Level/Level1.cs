844e78f4a5c3466cc602d8f308bfa0e47ba27e23
mehlicious <mehlicious@gmail.com>
2011-03-23 18:22:34 +0000

    bigger play

--- LegendOfDarwin/Level/Level1.cs
+++ LegendOfDarwin/Level/Level1.cs
@@ -76,7 +76,7 @@ namespace LegendOfDarwin
             gameState = new GameState();
             gameStart = new GameStart(device.PresentationParameters.BackBufferWidth, device.PresentationParameters.BackBufferHeight);
 
-            board = new GameBoard(new Vector2(25, 25), new Vector2(device.PresentationParameters.BackBufferWidth, device.PresentationParameters.BackBufferHeight));
+            board = new GameBoard(new Vector2(30, 25), new Vector2(device.PresentationParameters.BackBufferWidth, device.PresentationParameters.BackBufferHeight));
             darwin = new Darwin(board);
             firstZombie = new Zombie(10, 10, 15, 5, 15, 5, board);
             secondZombie = new Zombie(10, 16, 15, 5, 15, 5, board);
