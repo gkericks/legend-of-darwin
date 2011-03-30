93f9cc35f57afd653e3693d6cd7cffccdfa28b19
mehlicious@gmail.com <mehlicious@gmail.com@a3afd2dd-6ea3-eacb-4b53-07fd98ab0ad6>
2011-03-30 20:17:44 +0000

    added function stubs

--- trunk/LegendOfDarwin/GameObject/Nursery.cs
+++ trunk/LegendOfDarwin/GameObject/Nursery.cs
@@ -2,12 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
 
 namespace LegendOfDarwin.GameObject
 {
-    class Nursery
+    class Nursery : BasicObject
     {
+        private Texture2D nurseTex;
 
+        public Nursery(GameBoard gb)
+            : base(gb)
+        { 
+            
+        }
 
+        public void LoadContent(Texture2D texIn)
+        {
+            nurseTex = texIn;
+        }
+
+        public void Update(GameTime gameTime)
+        { 
+        
+        }
     }
 }
