57330d4aab0715353262b5e3a7c918b4e040383f
mehlicious <mehlicious@gmail.com>
2011-03-30 20:12:46 +0000

    Adding babyzombie and nursery classes

---
+++ LegendOfDarwin/GameObject/BabyZombie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace LegendOfDarwin.GameObject
+{
+    class BabyZombie : Zombie
+    {
+        private Darwin darwin;
+
+        public BabyZombie(int x, int y, int maxX, int minX, int maxY, int minY, Darwin dar, GameBoard gb) : 
+            base(x, y, maxX, minX, maxY, minY, gb)
+        {
+            darwin = dar;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            this.RandomWalk();
+        }
+    }
+}
