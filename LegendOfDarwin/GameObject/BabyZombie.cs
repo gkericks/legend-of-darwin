46feec965bbb39ce17b340f6aeb5720a07f48125
mehlicious <mehlicious@gmail.com>
2011-03-31 05:11:26 +0000

    Baby zombies now come outa the nursery and will explode on contact but do nothing

--- LegendOfDarwin/GameObject/BabyZombie.cs
+++ LegendOfDarwin/GameObject/BabyZombie.cs
@@ -11,30 +11,106 @@ namespace LegendOfDarwin.GameObject
     {
         private Darwin darwin;
 
+        private bool explode;
+        private Texture2D explodeTex;
+        private int explodeCount;
+        private Rectangle[] explodeSource;
+
         public BabyZombie(int x, int y, int maxX, int minX, int maxY, int minY, Darwin dar, GameBoard gb) : 
             base(x, y, maxX, minX, maxY, minY, gb)
         {
             darwin = dar;
-            this.setEventLag(30);
+
+            explode = false;
+            explodeCount = 0;
+            explodeSource = new Rectangle[3];
+            explodeSource[0] = new Rectangle(0, 0, 75, 90);
+            explodeSource[1] = new Rectangle(75, 0, 75, 90);
+            explodeSource[2] = new Rectangle(150, 0, 75, 90);
+
+            this.setEventLag(40);
         }
 
+        public void LoadContent(Texture2D babyIn, Texture2D splodeIn)
+        {
+            base.LoadContent(babyIn);
+            explodeTex = splodeIn;
+        }
 
         public new void Update(GameTime gameTime)
         {
             base.Update(gameTime);
-            if (this.isZombieAlive() && canEventHappen())
+            if (canEventHappen())
             {
-                this.moveTowardsDarwin(darwin);
+                if (explode)
+                {
+                    explodeCount++;
+
+
+                    if (explodeCount == 3)
+                    {
+                        explodeCount = 0;
+                        explode = false;
+                    }
+                    else
+                    {
+                        this.setZombieAlive(false);
+                    }
+                }
+                else if (isZombieAlive())
+                {
+                    this.moveTowardsDarwin(darwin);
+                    if (nearDarwin())
+                    {
+                        explode = true;
+                    }
+                }
                 this.setEventFalse();
             }
         }
 
         public new void Draw(SpriteBatch sp)
         {
+            if (explode)
+            {
+                sp.Draw(explodeTex, board.getPosition(this), explodeSource[explodeCount], Color.White);
+                sp.Draw(explodeTex, board.getPosition(this.X, this.Y + 1), explodeSource[explodeCount], Color.White);
+                sp.Draw(explodeTex, board.getPosition(this.X, this.Y - 1), explodeSource[explodeCount], Color.White);
+                sp.Draw(explodeTex, board.getPosition(this.X + 1, this.Y), explodeSource[explodeCount], Color.White);
+                sp.Draw(explodeTex, board.getPosition(this.X - 1, this.Y), explodeSource[explodeCount], Color.White);
+            }
             if (this.isZombieAlive())
             {
                 sp.Draw(this.zombieTexture, this.destination, Color.White);
             }
+        }
+
+        public bool nearDarwin()
+        {
+            int xDist, yDist;
+            if (darwin.X > this.X)
+            {
+                xDist = darwin.X - this.X;
+            }
+            else
+            {
+                xDist = this.X - darwin.X;
+            }
+
+            if (darwin.Y > this.Y)
+            {
+                yDist = darwin.Y - this.Y;
+            }
+            else
+            {
+                yDist = this.Y - darwin.Y;
+            }
+
+            if (xDist + yDist == 1)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
