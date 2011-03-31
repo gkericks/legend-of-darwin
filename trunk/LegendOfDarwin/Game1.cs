71cebb1102b999dfccd3fffa343ac20d9d072886
gke703@mail.usask.ca <gke703@mail.usask.ca@a3afd2dd-6ea3-eacb-4b53-07fd98ab0ad6>
2011-03-31 01:55:34 +0000

    added more conga followers, they atk darwin if he is nearby and human, or after a few seconds the leader will alert them if darwin is on dance floor and human

--- trunk/LegendOfDarwin/Game1.cs
+++ trunk/LegendOfDarwin/Game1.cs
@@ -36,7 +36,7 @@ namespace LegendOfDarwin
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
 
-            curLevel = LevelState.Level5;
+            curLevel = LevelState.Level4;
 
             level1 = new Level1(this);
             level2 = new Level2(this);
--- trunk/LegendOfDarwin/GameObject/CongaFollowerZombie.cs
+++ trunk/LegendOfDarwin/GameObject/CongaFollowerZombie.cs
@@ -13,6 +13,10 @@ namespace LegendOfDarwin.GameObject
         // darwin that is on the current game board
         protected Darwin darwin;
 
+        // used to delay zombie attack when spotted by CongaLeader
+        protected bool hasBeenSeen = false;
+        protected int hasBeenSeenCounter = 0;
+
         // array and index for designating pts at the vertices of the zombie's patrol path
         protected Vector2[] pathList;
         protected int pathCount = 0;
@@ -30,6 +34,180 @@ namespace LegendOfDarwin.GameObject
             visionMaxY = 4;
             darwin = mydarwin;
             pathList = myPathList;
+            ZOMBIE_MOVE_RATE = 30;
+        }
+
+        /*
+         * makes the zombie follow allong the designated patrol path
+         * */
+        public void followPath()
+        {
+            if (this.X == pathList[pathCount].X && this.Y == pathList[pathCount].Y)
+            {
+                pathCount++;
+            }
+
+            // reset path if neccessary
+            if (pathCount >= pathList.Length)
+            {
+                pathCount = 0;
+            }
+
+            moveTowardsPoint((int)pathList[pathCount].X, (int)pathList[pathCount].Y);
+
+        }
+
+        /*
+         * moves zombie towards a given point
+         * Used for straight line portions of the patrol path
+         * */
+        public void moveTowardsPoint(int ptX, int ptY)
+        {
+            int changeX = 0;
+            int changeY = 0;
+            int intendedPathX = 0;
+            int intendedPathY = 0;
+
+            changeX = ptX - this.X;
+            changeY = ptY - this.Y;
+
+            if (Math.Abs(changeX) > Math.Abs(changeY))
+            {
+                //move in x direction
+                if (ptX > this.X)
+                {
+                    //intend to move right
+                    intendedPathX = this.X + 1;
+                    intendedPathY = this.Y;
+
+                }
+                else if (ptX < this.X)
+                {
+                    //intend to move left
+                    intendedPathX = this.X - 1;
+                    intendedPathY = this.Y;
+                }
+            }
+            else
+            {
+                //move in y direction
+                if (ptY > this.Y)
+                {
+                    //intend to move down
+                    intendedPathX = this.X;
+                    intendedPathY = this.Y + 1;
+                }
+                else if (ptY < this.Y)
+                {
+                    //intend to move up
+                    intendedPathX = this.X;
+                    intendedPathY = this.Y - 1;
+                }
+            }
+
+            if (isZombieInRange(intendedPathX, intendedPathY))
+            {
+
+                bool canMoveThere = false;
+
+
+                if (darwin.X == intendedPathX && darwin.Y == intendedPathY && !darwin.isZombie())
+                    canMoveThere = true;
+
+                // checks for board position to be open
+                if (board.isGridPositionOpen(intendedPathX, intendedPathY) || canMoveThere)
+                {
+
+                    if (intendedPathX == this.X + 1)
+                        MoveRight();
+                    else if (intendedPathX == this.X - 1)
+                        MoveLeft();
+                    else if (intendedPathY == this.Y + 1)
+                        MoveDown();
+                    else if (intendedPathY == this.Y - 1)
+                        MoveUp();
+                }
+                else
+                {
+
+                    RandomWalk();
+
+                }
+            }
+
+        }
+
+        /*
+         * checks if darwin is on the dance floor or not
+         * that is, is darwin inside the patrol path
+         * Point must be set up so top left pt is first,
+         * bottom right pt is 3rd
+         * */
+        public bool isDarwinOnFloor(Darwin myDarwin)
+        {
+            // check for actual min.max points
+            Vector2 minPt = getMinPtOnPath();
+            Vector2 maxPt = getMaxPtOnPath();
+
+            if (myDarwin.X >= minPt.X && myDarwin.X <= maxPt.X && myDarwin.Y >= minPt.Y && myDarwin.Y <= maxPt.Y)
+                return true;
+            else
+                return false;
+        }
+
+        /*
+         * checks if darwin is on the conga line path or not
+         * that is, is darwin on the patrol path
+         * Point must be set up so top left pt is first,
+         * bottom right pt is 3rd
+         * */
+        public bool isDarwinOnPath(Darwin myDarwin)
+        {
+            // 
+            Vector2 minPt = getMinPtOnPath();
+            Vector2 maxPt = getMaxPtOnPath();
+
+            if ((myDarwin.X >= minPt.X && myDarwin.X <= maxPt.X && (myDarwin.Y == minPt.Y || myDarwin.Y == maxPt.Y))
+                || ((myDarwin.X == minPt.X || myDarwin.X == maxPt.X) && myDarwin.Y >= minPt.Y && myDarwin.Y <= maxPt.Y))
+                return true;
+            else
+                return false;
+        }
+
+        /* 
+         * accesses the pt on the zombies patrol path corresponding to the upper left corner
+         **/
+        public Vector2 getMinPtOnPath() 
+        {
+            Vector2 minPt = new Vector2(50, 50);
+            for (int i = 0; i < pathList.Length; i++)
+            {
+                if (pathList[i].X <= minPt.X && pathList[i].Y <= minPt.Y)
+                {
+                    minPt.X = pathList[i].X;
+                    minPt.Y = pathList[i].Y;
+                }
+            }
+
+            return minPt;
+        }
+
+        /* 
+         * accesses the pt on the zombies patrol path corresponding to the lower right corner
+         **/
+        public Vector2 getMaxPtOnPath()
+        {
+            Vector2 maxPt = new Vector2(0, 0);
+            for (int i = 0; i < pathList.Length; i++)
+            {
+                if (pathList[i].X >= maxPt.X && pathList[i].Y >= maxPt.Y)
+                {
+                    maxPt.X = pathList[i].X;
+                    maxPt.Y = pathList[i].Y;
+                }
+            }
+
+            return maxPt;
         }
 
         public new void Update(GameTime gameTime, Darwin darwin)
@@ -44,14 +222,34 @@ namespace LegendOfDarwin.GameObject
             if (movecounter > ZOMBIE_MOVE_RATE)
             {
 
-                if (this.isPointInVision(darwin.X,darwin.Y) && !darwin.isZombie())
+                if (this.isPointInVision(darwin.X, darwin.Y) && !darwin.isZombie() && isDarwinOnFloor(darwin))
                 {
                     this.enemyAlert = true;
                     source.X = 64;
-                    //moveTowardsPoint(darwin.X, darwin.Y);
+                    moveTowardsPoint(darwin.X, darwin.Y);
+                }
+                else if (isDarwinOnFloor(darwin) && !darwin.isZombie())
+                {
+                    if (!hasBeenSeen)
+                    {
+                        hasBeenSeenCounter++;
+                        if (hasBeenSeenCounter > 3)
+                            hasBeenSeen = true;
+                        followPath();
+                    }
+                    else
+                    {
+                        this.enemyAlert = true;
+                        source.X = 64;
+                        moveTowardsPoint(darwin.X, darwin.Y);
+                    }
+
                 }
                 else
                 {
+                    hasBeenSeen = false;
+                    hasBeenSeenCounter = 0;
+
                     if (enemyAlert)
                     {
                         source.X = 128;
@@ -65,7 +263,7 @@ namespace LegendOfDarwin.GameObject
                     else
                         this.source.X = 0;
 
-                    //followPath();
+                    followPath();
                 }
 
                 movecounter = 0;
