cc3f934da6abf662f6a4285fbc9d09d0adfaed21
rizzen93@gmail.com <rizzen93@gmail.com@a3afd2dd-6ea3-eacb-4b53-07fd98ab0ad6>
2011-04-01 00:46:58 +0000

    flamezombie stuff

--- trunk/LegendOfDarwin/GameObject/Flame.cs
+++ trunk/LegendOfDarwin/GameObject/Flame.cs
@@ -9,25 +9,35 @@ namespace LegendOfDarwin.GameObject
 {
     class Flame : BasicObject
     {
-
+        // static int for how long this flame should live
         private static int FLAME_LIFE = 150;
 
         // texture for the flame
         public Texture2D flameTex;
 
         // counter for how long this flame will burn a tile
-        public int flameCounter;
+        private int flameCounter;
 
-        // is this flame currently burning a tile?
-        private Boolean burning;
+        // is this flame alive
+        private Boolean alive;
 
         public Flame(GameBoard myboard, int x, int y)
             : base(myboard)
         {
             this.X = x;
             this.Y = y;
-            this.flameCounter = 1;
-            this.burning = true;
+            this.flameCounter = 0;
+            this.alive = true;
+        }
+
+        public void setAlive(Boolean state)
+        {
+            this.alive = state;
+        }
+
+        public Boolean isAlive()
+        {
+            return this.alive;
         }
 
         public void LoadContent(Texture2D flameTexture)
@@ -40,8 +50,7 @@ namespace LegendOfDarwin.GameObject
             if (this.flameCounter > FLAME_LIFE)
             {
                 // flame can die
-                this.burning = false;
-                this.flameCounter = 0;
+                this.alive = false;
             }
 
             this.flameCounter++;
