d159a894332e1ac883fa3bc6325ede67a784b694
gke703@mail.usask.ca <gke703@mail.usask.ca@a3afd2dd-6ea3-eacb-4b53-07fd98ab0ad6>
2011-04-06 19:07:56 +0000

    darwin deaths work on lvl 2

--- trunk/LegendOfDarwin/Game1.cs
+++ trunk/LegendOfDarwin/Game1.cs
@@ -41,7 +41,7 @@ namespace LegendOfDarwin
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
 
-            curLevel = LevelState.Level6;
+            curLevel = LevelState.Level2;
 
             level1 = new Level1(this);
             level2 = new Level2(this);
