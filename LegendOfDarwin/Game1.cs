121b4227887ac54a2894aa9e16a4c7f6d0f9dea8
gke703 <gke703@mail.usask.ca>
2011-04-08 06:11:54 +0000

    fixed problem in lvl2

--- LegendOfDarwin/Game1.cs
+++ LegendOfDarwin/Game1.cs
@@ -43,7 +43,7 @@ namespace LegendOfDarwin
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
 
-            curLevel = LevelState.Start;
+            curLevel = LevelState.Level2;
 
             level1 = new Level1(this);
             level2 = new Level2(this);
