d89121a6bd7cce877b001f1cfbdd6169756d3fc3
mehlicious@gmail.com <mehlicious@gmail.com@a3afd2dd-6ea3-eacb-4b53-07fd98ab0ad6>
2011-04-08 16:46:21 +0000

    pressing q doesn't quit game and escape quits now

--- trunk/LegendOfDarwin/Level/Level5.cs
+++ trunk/LegendOfDarwin/Level/Level5.cs
@@ -872,7 +872,7 @@ namespace LegendOfDarwin.Level
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                 mainGame.Exit();
 
-            if (ks.IsKeyDown(Keys.Q))
+            if (ks.IsKeyDown(Keys.Escape))
             {
                 mainGame.Exit();
             }
