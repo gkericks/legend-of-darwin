b7a0dd76e96b6670f73b6c65c3b31a945a6d9198
mehlicious@gmail.com <mehlicious@gmail.com@a3afd2dd-6ea3-eacb-4b53-07fd98ab0ad6>
2011-04-08 06:20:24 +0000

    Levels now show a more proper Death count and Jimanity

--- trunk/LegendOfDarwin/Level/Level4.cs
+++ trunk/LegendOfDarwin/Level/Level4.cs
@@ -2055,11 +2055,11 @@ namespace LegendOfDarwin.Level
             }
 
             spriteBatch.DrawString(messageFont, "HUMANITY: ",
-                new Vector2(board.getPosition(zTime.X, zTime.Y).X + board.getSquareWidth() * 2, board.getPosition(zTime.X, 24).Y),
+                new Vector2(board.getPosition(zTime.X, zTime.Y).X + board.getSquareWidth() * 3 / 2, board.getPosition(zTime.X, 24).Y),
                 Color.Black);
 
             spriteBatch.DrawString(messageFont, "DEATH COUNT: " + mainGame.DEATH_COUNTER.ToString(),
-                new Vector2(board.getPosition(15, 24).X, board.getPosition(15, 24).Y),
+                new Vector2(board.getPosition(23, 24).X, board.getPosition(23, 24).Y),
                 Color.Black);
 
             spriteBatch.End();
